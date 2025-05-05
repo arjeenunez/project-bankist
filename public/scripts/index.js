4bec3c1990e60dc0fb34474a5dce35e823e8340b
Arjee <arjeenunez@gmail.com>
2025-05-05 18:00:24 +0800

    Refactored most of the code, revised the logout timer, added functionality to all forms

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Models
+
 const user1 = {
     name: 'Jeffrey Knight',
     username: 'jk',
@@ -36,51 +38,44 @@ const user2 = {
     pin: 1234,
 };
 
-const users = [user1, user2];
+let users = [user1, user2];
 let currentUser = null;
 
-const loginForm = document.querySelector('.loginForm');
-const logoutForm = document.querySelector('.logoutForm');
-const mainContainer = document.querySelector('.mainContainer');
-const formText = document.querySelector('.form-text');
-const currentDate = document.querySelector('.currentDate');
-const totalBalance = document.querySelector('.totalBalance');
-const transactionList = document.querySelector('.transactionList');
-const totalIn = document.querySelector('.totalIn');
-const totalOut = document.querySelector('.totalOut');
-const totalInterest = document.querySelector('.totalInterest');
-const countdown = document.querySelector('.countdown');
-const transferMoneyForm = document.querySelector('.transferMoney');
-const requestLoanForm = document.querySelector('.requestLoan');
-const closeAccountForm = document.querySelector('.closeAccount');
+const userUpdate = function (user, amount = null, date = new Date()) {
+    if (amount) {
+        user.movements.push(amount);
+        user.movementDates.push(date);
+    }
+    user.total = user.movements.reduce((total, el) => total + el);
+    user.sumIn = user.movements.filter(el => el > 0).reduce((total, el) => total + el);
+    user.sumOut = user.movements.filter(el => el < 0).reduce((total, el) => total + Math.abs(el), 0);
+    user.sumInterest = user.sumIn * (user.interestRate / 100);
+};
 
-const updateCurrentDate = function () {
+const userDelete = function (user) {
+    users = users.filter(el => el.username !== user.username);
+};
+
+// Views
+
+const viewCurrentLoginText = function (name) {
+    const formText = document.querySelector('.form-text');
+    formText.textContent = `Welcome back, ${name}!`;
+};
+
+const viewCurrentDate = function () {
+    const currentDate = document.querySelector('.currentDate');
     const date = new Date();
     currentDate.textContent = date;
 };
 
-const updateTotalBalance = function () {
-    const total = currentUser.movements.reduce((total, el) => total + el);
-    console.log(total, currentUser);
-    totalBalance.textContent = `${total}`;
+const viewCurrentTotalBalance = function (user) {
+    const totalBalance = document.querySelector('.totalBalance');
+    totalBalance.textContent = `${user.total}€`;
 };
 
-const updateTransactions = function () {
-    transactionList.innerHTML = '';
-    for (let i = currentUser.movements.length - 1; i >= 0; i--) {
-        const transaction = createTransactionItem(currentUser.movements[i], currentUser.movementDates[i], i + 1);
-        transactionList.append(transaction);
-    }
-};
-
-const addTransaction = function (price, date) {
-    currentUser.movements.push(price);
-    currentUser.movementDates.push(date);
-    const transaction = createTransactionItem(price, date, currentUser.movements.length);
-    transactionList.prepend(transaction);
-};
-
-const createTransactionItem = function (price, date, transNum) {
+const createTransaction = function (price, transNum, date = new Date()) {
+    const transactionList = document.querySelector('.transactionList');
     const li = document.createElement('li');
     const div = document.createElement('div');
     const span1 = document.createElement('span');
@@ -95,77 +90,141 @@ const createTransactionItem = function (price, date, transNum) {
     span3.textContent = Math.abs(price);
     div.append(span1, span2);
     li.append(div, span3);
-    return li;
+    transactionList.prepend(li);
 };
 
-const updateTotals = function () {
-    const sumIn = currentUser.movements.filter(el => el > 0).reduce((total, el) => total + el);
-    const sumOut = currentUser.movements.filter(el => el < 0).reduce((total, el) => total + Math.abs(el), 0);
-    const sumInterest = sumIn * (currentUser.interestRate / 100);
-    totalIn.textContent = sumIn;
-    totalOut.textContent = sumOut;
-    totalInterest.textContent = sumInterest.toFixed(2);
+const clearTransactions = function () {
+    const transactionList = document.querySelector('.transactionList');
+    transactionList.innerHTML = '';
+};
+
+const viewCurrentSummary = function (user) {
+    const totalIn = document.querySelector('.totalIn');
+    const totalOut = document.querySelector('.totalOut');
+    const totalInterest = document.querySelector('.totalInterest');
+    totalIn.textContent = user.sumIn;
+    totalOut.textContent = user.sumOut;
+    totalInterest.textContent = user.sumInterest.toFixed(2);
+};
+
+const viewCurrentTimer = function (minutes = 10, seconds = 0) {
+    const countdown = document.querySelector('.countdown');
+    let mins = minutes < 10 ? `0${minutes}` : minutes;
+    let secs = seconds < 10 ? `0${seconds}` : seconds;
+    countdown.textContent = `You'll be automatically logged out in ${mins}:${secs}`;
 };
 
 const toggleLoginElements = function () {
+    const mainContainer = document.querySelector('.mainContainer');
     mainContainer.classList.toggle('d-none');
     loginForm.classList.toggle('d-none');
     logoutForm.classList.toggle('d-none');
-    // mainContainer.classList.toggle('hidden');
-    // loginForm.classList.toggle('hidden');
-    // logoutForm.classList.toggle('hidden');
 };
 
-const userLoggedOut = () => {
-    currentUser = null;
+// Controllers
+
+const loggedIn = user => {
+    userUpdate(user);
     toggleLoginElements();
+    viewCurrentDate();
+    viewCurrentTotalBalance(user);
+    viewCurrentSummary(user);
+    clearTransactions();
+    viewCurrentLoginText(user.name);
+    for (let i = 0; i < user.movements.length; i++) {
+        createTransaction(user.movements[i], i + 1, user.movementDates[i]);
+    }
 };
 
-const logoutTimer = function () {
+const loggedOut = () => {
+    clearTransactions();
+    toggleLoginElements();
+    viewCurrentTimer();
+};
+
+let intervalCountdown = null;
+
+const logoutTimerEnd = () => clearInterval(intervalCountdown);
+
+const logoutTimerStart = function () {
     const date = Date.now() + 10 * 60 * 1000 + 1000;
-    const autoLogout = setInterval(() => {
+    intervalCountdown = setInterval(() => {
         let dateNow = Date.now();
         let timeLeft = date - dateNow;
         const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-        countdown.textContent = `You'll be automatically logged out in ${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-        if (dateNow > date) {
-            userLoggedOut();
-            countdown.textContent = `You'll be automatically logged out in 10:00`;
-            clearInterval(autoLogout);
+        viewCurrentTimer(minutes, seconds);
+        if (dateNow > date - 1000) {
+            currentUser = null;
+            toggleLoginElements();
+            logoutTimerEnd();
         }
     }, 1000);
 };
 
-const userLoggedIn = foundUser => {
-    currentUser = foundUser;
-    formText.textContent = `Welcome back, ${currentUser.name}!`;
-    toggleLoginElements();
-    updateCurrentDate();
-    updateTotalBalance();
-    updateTransactions();
-    updateTotals();
-    logoutTimer();
-};
+// Controllers - events
+
+const loginForm = document.querySelector('.loginForm');
+const logoutForm = document.querySelector('.logoutForm');
+const requestLoanForm = document.querySelector('.requestLoan');
+const transferMoneyForm = document.querySelector('.transferMoney');
+const closeAccountForm = document.querySelector('.closeAccount');
 
 loginForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
     const usernameInput = document.querySelector('.usernameInput');
     const passwordInput = document.querySelector('.passwordInput');
-    let foundUser = users.find(el => el.username === usernameInput.value);
-    if (foundUser && foundUser.pin === +passwordInput.value) userLoggedIn(foundUser);
+    const foundUser = users.find(el => el.username === usernameInput.value);
+    if (foundUser && foundUser.pin === +passwordInput.value) {
+        currentUser = foundUser;
+        loggedIn(currentUser);
+        logoutTimerStart();
+    }
     usernameInput.value = '';
     passwordInput.value = '';
 });
 
 logoutForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    userLoggedOut();
+    loggedOut(currentUser);
+    logoutTimerEnd();
 });
 
 requestLoanForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
     const loanAmount = document.querySelector('.loanAmount');
-    const date = new Date();
-    addTransaction(+loanAmount.value, date);
+    userUpdate(currentUser, +loanAmount.value);
+    viewCurrentSummary(currentUser);
+    viewCurrentTotalBalance(currentUser);
+    createTransaction(+loanAmount.value, currentUser.movements.length);
+    loanAmount.value = '';
+});
+
+transferMoneyForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    const transferUser = document.querySelector('.transferUser');
+    const transferAmount = document.querySelector('.transferAmount');
+    const foundUser = users.find(el => el.username === transferUser.value);
+    if (foundUser && foundUser.username !== currentUser.username && +transferAmount.value <= currentUser.total && +transferAmount.value > 0) {
+        userUpdate(currentUser, -Number(transferAmount.value));
+        userUpdate(foundUser, +transferAmount.value);
+        viewCurrentSummary(currentUser);
+        viewCurrentTotalBalance(currentUser);
+        createTransaction(-Number(transferAmount.value), currentUser.movements.length);
+    }
+    transferAmount.value = '';
+    transferUser.value = '';
+});
+
+closeAccountForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    const closeUsername = document.querySelector('.closeUsername');
+    const closePassword = document.querySelector('.closePassword');
+
+    const foundUser = users.find(el => el.username === closeUsername.value);
+    if (foundUser && foundUser.username === currentUser.username && +closePassword.value === currentUser.pin) {
+        userDelete(currentUser);
+        logoutTimerEnd();
+        loggedOut(currentUser);
+    }
 });
